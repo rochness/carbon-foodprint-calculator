f8274d1f22133e7e19de75abf84f60e4c246de06
Rochelle Lee <rochelle.y.lee@gmail.com>
2015-11-09 19:48:54 -0800

    Completed implementation for displaying total emissions and equivalent in miles driven and offset amount

--- client/app/calculate/calculate.js
+++ client/app/calculate/calculate.js
@@ -1,7 +1,8 @@
 angular.module('calculator.userIngreds', [])
 
 .controller('UserIngredController', function ($scope, Ingredients) {
-  $scope.Ingredients = Ingredients;
+  
+  //$scope.Ingredients = Ingredients;
 
   $scope.getUserIngreds = function(){
     return Ingredients.getUserIngreds();
@@ -12,9 +13,26 @@ angular.module('calculator.userIngreds', [])
     $scope.getUserIngreds();
   });
 
-  // $scope.calculate = function(){
-  //   $scope.total = $scope.ingredient.emissions * $scope.quantity;
-  // };
+  $scope.total = function(){
+    var total = 0;
+    var ingredients = Ingredients.getUserIngreds();
+    for(var i = 0; i < ingredients.length; i++){
+      total += ingredients[i].quantity * ingredients[i].ingredient.emissions;
+    }
+    return Math.ceil(total * (1/2.2046) * 100)/100;
+  };
+
+  $scope.miles = function(){
+    return Math.ceil($scope.total()*(5/2)*100)/100;
+  };
+
+  $scope.subTotal = function(qty, emiss){
+    return Math.ceil(qty * emiss * (1/2.2046) * 100)/100;
+  };
+
+  $scope.costs = function(qty, emiss){
+    return Math.ceil($scope.total() * (1/4) * 100)/100;
+  };
 
   $scope.getUserIngreds(); 
 
